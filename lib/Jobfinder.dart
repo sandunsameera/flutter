ae885351b633752e9e82d26ca896bfda209ee4ea
sandunsameera <sandunsameera25@gmail.com>
2019-06-01 14:48:30 +0530

    Job finder update

---
+++ lib/Jobfinder.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class Jobfinder extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Text(
+          "Job finder",
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            ListTile(
+              title: Text("Assistant software Engineer"),
+              trailing: Text(
+                "Apply",
+                style: TextStyle(
+                  color: Colors.green,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
